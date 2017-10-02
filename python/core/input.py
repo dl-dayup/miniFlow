fcdb4403cc623afb93485d36121809e8e6c744e1
zhengying1 <zhengying1@staff.weibo.com>
2017-10-02 13:33:24 +0800

    init

---
+++ python/core/input.py
@@ -0,0 +1,28 @@
+import numpy as np
+from node import Node
+
+class Input(Node):
+    """
+    A generic input into the network.
+    """
+    def __init__(self):
+        # The base class constructor has to run to set all
+        # the properties here.
+        #
+        # The most important property on an Input is value.
+        # self.value is set during `topological_sort` later.
+        Node.__init__(self)
+
+    def forward(self):
+        # Do nothing because nothing is calculated.
+        pass
+
+    def backward(self):
+        # An Input node has no inputs so the gradient (derivative)
+        # is zero.
+        # The key, `self`, is reference to this object.
+        self.gradients = {self: 0}
+        # Weights and bias may be inputs, so you need to sum
+        # the gradient from output gradients.
+        for n in self.outbound_nodes:
+            self.gradients[self] += n.gradients[self]
